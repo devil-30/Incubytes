36d60a8cd9d361e7fab21eafc93a1cb9d145ef55
Vijay <duleravijay903@gmail.com>
2021-08-29 23:32:39 +0530

    Second Commit

--- calcy.java
+++ calcy.java
@@ -4,13 +4,25 @@ class StringCalculator
 {
     public int Add(String numbers)
     {
-
+        if(numbers==null)
+            System.out.println("0");
+        else
+        {
+            
+        }
     }
 }
 class calcy
 {
     public static void main(String args[])
     {
-        
+        try{
+            int op;
+            Scanner scan=new Scanner(System.in);
+            StringCalculator sc=new StringCalculator();
+            System.out.print("Enter String:- ");
+            String s=scan.next();
+            op=sc.Add(s);
+        }catch(Exception e){System.out.println(e);}
     }
 }
