002b2a13da11727bbd156c41418a4ccfb5f4e2ac
Vijay <duleravijay903@gmail.com>
2021-08-29 23:54:34 +0530

    Fifth Commit

--- calcy.java
+++ calcy.java
@@ -5,7 +5,7 @@ class StringCalculator
     public int Add(String numbers)
     {
         if(numbers==null)
-            System.out.println("0");
+            return 0;
         else
         {
             ArrayList<Integer> NArray=new ArrayList<Integer>();
@@ -18,16 +18,19 @@ class StringCalculator
                     NArray.add(a);
                 }
             }
-            System.out.println(NArray);
             int sum=0;
             for(Integer value:NArray)
             {
                 if(value<=1000)
                     sum+=value;
             }
-            System.out.println(sum);
+            return sum;
         }
-        return 0;
+    }
+    int count=1;
+    public int GetCalledCount()
+    {
+        return count++;
     }
 }
 class calcy
@@ -35,12 +38,22 @@ class calcy
     public static void main(String args[])
     {
         try{
-            int op;
+            int op,c,choice=1,y=1;
             Scanner scan=new Scanner(System.in);
             StringCalculator sc=new StringCalculator();
-            System.out.print("Enter String:- ");
-            String s=scan.next();
-            op=sc.Add(s);
+            while(choice==1 && y==1)
+            {
+                System.out.print("Enter String:- ");
+                String s=scan.next();
+                op=sc.Add(s);
+                c=sc.GetCalledCount();
+                System.out.println("Sum of Integers is "+op);
+                System.out.println(c+" times Add() was invoked");
+                try{
+                    System.out.println("Press 1 for Continue and 2 for Exit");
+                    choice=scan.nextInt();
+                }catch(InputMismatchException b){y=1;}
+            }
         }catch(Exception e){System.out.println(e);}
     }
 }
